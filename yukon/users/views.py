fc12234617fb972ad2ad59dc7c20cac641c53c6f
SolArt <artemghug5@gmail.com>
2023-02-16 19:42:19 +0200

    +registration

---
+++ yukon/users/views.py
@@ -0,0 +1,32 @@
+from django.contrib.auth import authenticate, login
+from django.shortcuts import render, redirect
+from users.forms import UserCreationForm
+from django.views import View
+
+
+class Register(View):
+
+    templates_name = 'registration/register.html'
+
+    def get(self, request):
+
+        context = {
+            'form': UserCreationForm()
+        }
+        return render(request, self.templates_name, context)
+
+
+    def post(self, request):
+        form = UserCreationForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password1')
+            user = authenticate(username=username,  password=password)
+            login(request, user)
+            return redirect('home')
+        contex = {
+            'form': form
+        }
+        return render(request, self.templates_name, contex)
